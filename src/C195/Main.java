f0f802468b61b4cc1f460a061ad03b0d1c9bda54
LabUser <LabUser@DESKTOP-BT4131D>
2023-08-03 12:24:22 -0600

    Real Initial Commit

---
+++ src/C195/Main.java
@@ -0,0 +1,25 @@
+package C195;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author LabUser
+ */
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("C195 Appoitment Schedulder");
+        stage.show();
+    }
+}
